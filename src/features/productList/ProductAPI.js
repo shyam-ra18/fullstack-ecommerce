7bac81957e04c27ddcecd906ad05ad19b0ee172a
Shyam Radadiya <shyam.radadiya100@gmail.com>
2023-06-18 22:37:57 +0000

    fake api integrate. filter & sorting product done

--- src/features/productList/ProductAPI.js
+++ src/features/productList/ProductAPI.js
@@ -1,8 +1,30 @@
-// A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
+import axios from "axios";
+
+export function fetchAllProducts() {
   return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:8000");
-    const data = await response.json();
+    const response = await axios.get("http://localhost:8080/products");
+    const data = response.data;
+    resolve({ data });
+  });
+}
+
+export function fetchProductsByFilters(filter, sort) {
+  let queryString = "";
+  for (let key in filter) {
+    const categoryValues = filter[key];
+    if (categoryValues.length) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
+  }
+  // for (let key in sort) {
+  //   queryString += `${key}=${lastCategoryValue}&`;
+  // }
+  return new Promise(async (resolve) => {
+    const response = await axios.get(
+      `http://localhost:8080/products?${queryString}`
+    );
+    const data = response.data;
     resolve({ data });
   });
 }
